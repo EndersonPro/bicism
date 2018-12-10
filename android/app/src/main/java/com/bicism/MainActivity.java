bca55c4078e9e1867246cb656a6475a40cb1be63
JoralmoPro <JoralmoPro@gmail.com>
2018-12-10 00:21:15 -0500

    Nuevo logo, nuevo nombre 🕶️

--- android/app/src/main/java/com/bicism/MainActivity.java
+++ android/app/src/main/java/com/bicism/MainActivity.java
@@ -1,4 +1,4 @@
-package com.logincli2;
+package com.bicism;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -14,7 +14,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "loginCli2";
+        return "BiciSM";
     }
 
     @Override
@@ -26,4 +26,5 @@ public class MainActivity extends ReactActivity {
             }
         };
     }
+
 }
--- android/app/src/main/java/com/bicism/MainApplication.java
+++ android/app/src/main/java/com/bicism/MainApplication.java
@@ -1,4 +1,4 @@
-package com.logincli2;
+package com.bicism;
 
 import android.app.Application;
 
@@ -14,6 +14,7 @@ import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,13 +29,14 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new MapsPackage(),
-          new VectorIconsPackage(),
-          new RNGestureHandlerPackage(),
-          new RNFirebasePackage(),
-          new RNFirebaseAuthPackage(),
-          new RNFirebaseFirestorePackage()
+        new MainReactPackage(),
+        new MapsPackage(),
+        new VectorIconsPackage(),
+        new RNGestureHandlerPackage(),
+        new RNFirebasePackage(),
+        new RNFirebaseAuthPackage(),
+        new RNFirebaseFirestorePackage()
+
       );
     }
 
