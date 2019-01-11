cdf491539db75e91350ea085d688425334602845
Enderson Vizcaino <enjaviga@gmail.com>
2019-01-11 17:25:38 -0500

    Subiendo todo funcionando

--- bicism/src/index.js
+++ bicism/src/index.js
@@ -23,16 +23,24 @@ const store = createStore(rootReducer,
 );
 
 store.firebaseAuthIsReady.then(() => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <App />
-        </Provider>
-        , document.getElementById('root'), () => {
-            var elems = document.querySelectorAll('.sidenav');
-            var instances = M.Sidenav.init(elems);
-        });
 
+
+    let loadSidenav = () => {
+        var elems = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(elems);
+    }
+
+    ReactDOM.render(
+        <Provider store={store}><App /></Provider>
+        , document.getElementById('root'),
+        loadSidenav);
+
+    
+    
 })
+
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
